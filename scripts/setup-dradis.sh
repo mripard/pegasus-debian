89aa3052dd7598e886dd9f282cb394538f993c86
Maxime Ripard <mripard@kernel.org>
2025-03-25 15:49:08 +0100

    Update Dradis
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- scripts/setup-dradis.sh
+++ scripts/setup-dradis.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 VERSION=$1
-FILENAME=dradis_${VERSION}-1_arm64.deb
+FILENAME=dradis-raspberrypi4_${VERSION}-1_arm64.deb
 URL=https://github.com/mripard/dradis/releases/download/v${VERSION}/${FILENAME}
 
 wget ${URL}
